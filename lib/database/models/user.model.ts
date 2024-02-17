03b6f279a480dc07967acef73afa364207f36a5f
Noway94 <hamolol@hotmail.com>
2024-02-17 20:15:23 +0200

    fixed errors

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,6 +1,6 @@
 import { Schema, model, models } from "mongoose";
 
-<<<<<<< HEAD
+
 const UserSchema = new Schema({
   clerkId: {
     type: String,
@@ -35,36 +35,7 @@ const UserSchema = new Schema({
     type: Number,
     default: 10,
   },
-=======
-/*
-export interface IUser extends Document {
-    clerkId: string;
-    email: string;
-    username: string;
-    photo: String;
-    firstName: string;
-    lastName: string;
-    planId: string;
-    creditBalance: number;
-    createdAt?: Date;
-    updatedAt?: Date;
-}
-*/
-  
-
-
-const UserSchema = new Schema({
-    clerkId: {type: String, required: true ,unique:true},
-    email: {type: String, required: true ,unique:true},
-    username: {type: String, required: true ,unique:true},
-    photo: {type: String, required: true},
-    firstName: {type: String},
-    lastName: {type: String},
-    planId: {type: String,defualt:1},
-    creditBalance: {type: Number,defualt:10},
-
->>>>>>> origin/main
-});
+})
 
 const User = models?.User || model("User", UserSchema);
 
